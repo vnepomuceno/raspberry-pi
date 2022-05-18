68fb23d1b302b821efc152f5f57c60b1a471ca71
Valter Nepomuceno <valter.nep@gmail.com>
2022-05-18 19:28:06 +0100

    Update motd

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 # raspberry-pi
+
+Install:
+
+* [Docker](https://pimylifeup.com/raspberry-pi-docker)
+* [ZSH](https://www.seeedstudio.com/blog/2020/03/06/prettify-raspberry-pi-shell-with-oh-my-zsh)
--- motd/motd.sh
+++ motd/motd.sh
@@ -1,3 +1,5 @@
+#!/bin/bash
+
 let upSeconds="$(/usr/bin/cut -d. -f1 /proc/uptime)"
 let secs=$((${upSeconds}%60))
 let mins=$((${upSeconds}/60%60))
@@ -9,14 +11,14 @@ UPTIME=`printf "%d days, %02dh%02dm%02ds" "$days" "$hours" "$mins" "$secs"`
 read one five fifteen rest < /proc/loadavg
 
 echo "$(tput setaf 2)
-   .~~.   .~~.    `date +"%A, %e %B %Y, %r"`
-  '. \ ' ' / .'   `uname -srmo`$(tput setaf 1)
-   .~ .~~~..~.    
-  : .~.'~'.~. :   Uptime.............: ${UPTIME}
- ~ (   ) (   ) ~  Memory.............: `cat /proc/meminfo | grep MemFree | awk {'print $2'}`kB (Free) / `cat /proc/meminfo | grep MemTotal | awk {'print $2'}`kB (Total)
-( : '~'.~.'~' : ) Load Averages......: ${one}, ${five}, ${fifteen} (1, 5, 15 min)
- ~ .~ (   ) ~. ~  Running Processes..: `ps ax | wc -l | tr -d " "`
-  (  : '~' :  )   IP Addresses.......: `/sbin/ifconfig eth0 | /bin/grep "inet addr" | /usr/bin/cut -d ":" -f 2 | /usr/bin/cut -d " " -f 1` and `wget -q -O - http://icanhazip.com/ | tail`
-   '~ .~~~. ~'    Weather............: `curl -s "http://rss.accuweather.com/rss/liveweather_rss.asp?metric=1&locCode=EUR|UK|UK001|NAILSEA|" | sed -n '/Currently:/ s/.*: \(.*\): \([0-9]*\)\([CF]\).*/\2°\3, \1/p'`
-       '~'
+   .~~.   .~~.         `date +"%A, %e %B %Y, %r"`
+  '. \ ' ' / .'        `uname -srmo`$(tput setaf 1)
+   .~ .~~~..~.         
+  : .~.'~'.~. :        OS Name............: `cat /etc/os-release | grep PRETTY_NAME | cut -c 14- | rev | cut -c 2- | rev`
+ ~ (   ) (   ) ~       Uptime.............: ${UPTIME}
+( : '~'.~.'~' : )      Memory.............: `cat /proc/meminfo | grep MemFree | awk {'print $2'}`kB (Free) / `cat /proc/meminfo | grep MemTotal | awk {'print $2'}`kB (Total)
+ ~ .~ (   ) ~. ~       Load Averages......: ${one}, ${five}, ${fifteen} (1, 5, 15 min)
+  (  : '~' :  )        Running Processes..: `ps ax | wc -l | tr -d " "`
+   '~ .~~~. ~'         IP Addresses.......: `/sbin/ifconfig eth0 | /bin/grep "inet addr" | /usr/bin/cut -d ":" -f 2 | /usr/bin/cut -d " " -f 1` and `wget -q -O - http://icanhazip.com/ | tail`
+       '~'             Weather............: `curl -s "http://rss.accuweather.com/rss/liveweather_rss.asp?metric=1&locCode=EUR|UK|UK001|NAILSEA|" | sed -n '/Currently:/ s/.*: \(.*\): \([0-9]*\)\([CF]\).*/\2°\3, \1/p'`
 $(tput sgr0)"
